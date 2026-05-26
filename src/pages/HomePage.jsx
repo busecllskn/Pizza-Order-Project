86286f4c01b5c15f4d4cc9e8a8d0cd678fe8d259
busecllskn <buseclskn738@gmail.com>
2026-05-26 03:01:18 +0300

    homepage button update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,8 +6,7 @@ export default function HomePage() {
   return (
     <div className="hero" style={{ backgroundImage: `url(${homeImg})` }}>
       <div className="hero-content">
-        <p className="brand">Teknolojik Yemekler</p>
-        <h1>KOD ACIKTIRIR<br />PİZZA, DOYURUR</h1>
+     
         <Link to="/order">
           <button className="hero-btn">ACIKTIM</button>
         </Link>
